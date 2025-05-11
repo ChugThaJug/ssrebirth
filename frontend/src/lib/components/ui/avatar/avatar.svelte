8a4058de8fdf212a4fd55475017cadb658d498b6
thebrandedman <40077401+ChugThaJug@users.noreply.github.com>
2025-05-11 16:09:11 +0500

    update 2.1

---
+++ frontend/src/lib/components/ui/avatar/avatar.svelte
@@ -0,0 +1,9 @@
+<!-- src/lib/components/ui/avatar/avatar.svelte -->
+<script lang="ts">
+  let className = '';
+  export { className as class };
+</script>
+
+<div class={`relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full ${className}`}>
+  <slot />
+</div>
